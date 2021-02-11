3904820573ea53986c06aed991799a07b44698a4
Luca Motta <l.motta@reply.it>
2021-02-11 10:36:30 +0100

    Lesson 5 File-based Routing complete

---
+++ pages/event/_id.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Event #{{ this.$route.params.id }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+	//"head" property used by vue-meta plugin
+	head() {
+		return {
+			title: "Event #" + this.id,
+			meta: [{
+				hid: "description",
+				name: "description", // <-- for our meta description tag
+				content: "What you need to know about event #" + this.id
+			}]
+		}
+	},
+	computed: {
+		id() {
+			return this.$route.params.id
+		}
+	},
+}
+</script>
