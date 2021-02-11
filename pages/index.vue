25511d7d03bd7edc86e1ff2a9e9bb8960d9fae89
Luca Motta <l.motta@reply.it>
2021-02-11 11:13:44 +0100

    Lesson 6 API Calls with Axios complete

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,41 @@
 <template>
   <div >
     <h1>Events</h1>
+	<EventCard 
+		v-for="(event, index) in events"
+		:key="index"
+		:event="event"
+		:data-index="index"/>
   </div>
 </template>
 
+
 <script>
+import EventCard from "@/components/EventCard.vue"
+
 export default {
+	components: {
+		EventCard
+	},
 	//"head" property used by vue-meta plugin
 	head() {
 		return {
 			title: "Event Listing"
 		}
+	},
+	asyncData({ $axios, error }) {
+		return $axios.get("http://localhost:3000/events")
+			.then(response => {
+				return {
+					events: response.data
+				}
+			})
+			.catch(() => {
+				error({
+					statusCode: 503,
+					message: "Unable to fetch events at this time. Please try again."
+				})
+			})
 	},
 }
 </script>
