65304cefc301d39891580d4bb9a80b8ef2aec78a
Luca Motta <l.motta@reply.it>
2021-02-11 09:23:32 +0100

    Add ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	parserOptions: {
+		parser: "babel-eslint"
+	},
+	extends: ["plugin:vue/essential",
+		"eslint:recommended", "plugin:nuxt/recommended"],
+	plugins: [],
+	// add your custom rules here
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "windows"],
+		quotes: ["error", "double"],
+		semi: ["error", "never"]
+	}
 }
